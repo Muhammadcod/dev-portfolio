845e05b44768f4ef5e658a09c35e2087f5aa27c2
Adebayo Mohammed Olusiji <adebayomuhammad47@gmail.com>
2020-09-22 00:55:19 -0700

    feat: Building UI

---
+++ src/pages/about.js
@@ -0,0 +1,54 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="About" />
+    <div className="about-page-content">
+      <section id="story">
+        <div className="container">
+          <div className="sub-title">
+            <h1>My Story</h1>
+          </div>
+          <div className="story-paragraph">
+            Adipiscing cras cursus eleifend est malesuada molestie nunc
+            phasellus porta posuere suspendisse ut. Aenean arcu at bibendum
+            egestas eget eros facilisi fringilla integer lectus mattis mollis
+            nisl pharetra risus semper torquent. Accumsan convallis cubilia
+            curae; dapibus dignissim dui duis eget facilisi fringilla iaculis
+            laoreet libero lobortis luctus mus parturient porta semper ultrices
+            vehicula velit vivamus viverra. Ac bibendum elit justo leo libero
+            magnis sagittis sociosqu ut vestibulum. Ante arcu convallis cras
+            dolor eros hac leo natoque nisl quis sociis ultricies. A ac
+            condimentum consectetur cras elementum facilisi iaculis justo
+            lacinia magnis mus parturient placerat platea rutrum sed semper
+            sociis sociosqu suspendisse tempus ullamcorper velit vestibulum
+            vitae vivamus. Aliquam condimentum curae; dictumst dui eu facilisis
+            hendrerit magnis nam non parturient tincidunt ultrices urna vel. Ad
+            adipiscing amet consequat convallis eget fames feugiat habitasse
+            himenaeos lectus ligula lobortis malesuada mi nibh nisl ornare
+            platea pulvinar rutrum sapien tellus torquent turpis vestibulum.
+            Adipiscing augue congue duis habitasse luctus massa metus nascetur
+            odio tempus urna vitae. Aenean blandit cum curabitur dolor iaculis
+            justo lobortis massa mauris molestie mollis nam non nunc odio orci
+            parturient pellentesque placerat porta quisque sodales ullamcorper
+            urna velit viverra. Adipiscing cras cursus eleifend est malesuada
+            molestie nunc phasellus porta posuere suspendisse ut. Aenean arcu at
+            bibendum egestas eget eros facilisi fringilla integer lectus mattis
+            mollis nisl pharetra risus semper torquent. Accumsan convallis
+            cubilia curae; dapibus dignissim dui duis eget facilisi fringilla
+            iaculis laoreet libero lobortis luctus mus parturient porta semper
+            ultrices vehicula velit vivamus viverra. Ac bibendum elit justo leo
+          </div>
+        </div>
+      </section>
+      <section id="skill"></section>
+      <section id="works"></section>
+    </div>
+  </Layout>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,118 @@
 import React from "react"
-import { Link } from "gatsby"
-
+// import { Link } from
+// import Img from "gatsby-image"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <section id="hero">
+      <div className="container hero-header p-0">
+        <p>Hi, my name is</p>
+        <h2>
+          Muhammad Bashir. <br /> I build things for the web.
+        </h2>
+        <p className="hero-description">
+          I'm a front end developer based in Lagos, specializing <br />
+          in building exceptional websites, applications, and <br />
+          everything in between.
+        </p>
+      </div>
+      <span className="socials container ">
+        <li>GitHub</li>
+        <li>LinkedIn</li>
+        <li>Twitter</li>
+      </span>
+    </section>
+    <section>
+      <div className="title border">Recent Works</div>
+      <div className="container px-5 border">
+        <div className="container way">
+          <div class="row  p-lg-2 p-sm-0 p-md-2 position-relative">
+            <div class="col-lg-4 col-12 tell two">
+              <div className="work-card">
+                <p className="work-title">Online Store</p>
+                {/* <hr /> */}
+                {/* <div className="h1 code mt-4 mb-3">async {"{"}</div>  */}
+                <p className="work-description">
+                  Adipiscing cras cursus eleifend est malesuada molestie nunc
+                  phasellus porta posuere suspendisse ut. Aenean arcu at
+                  bibendum egestas eget eros facilisi fringilla integer lectus
+                  mattis mollis nisl pharetra risus semper torquent.
+                </p>
+              </div>
+              {/* <hr /> */}
+              <div className="work-tech">
+                <li>Gatsby</li>
+                <li>React</li>
+                <li>API</li>
+              </div>
+            </div>
+            <div class="col-lg-8 col-12 border two sky">B</div>
+          </div>
+        </div>
+      </div>
+      <div className="title border">Other Works</div>
+      <div class="container">
+        <div class="row row-cols-1 row-cols-lg-2 g-2 g-lg-3">
+          <div class="col">
+            <div class="p-3 border project-wrap">
+              <h4 className="">Title</h4>
+              <p className="description">
+                Find out how we built a custom headless CMS with Node, Express,
+                and Firebase for a project at Upstatement
+              </p>
+              <div className="technology">
+                <a className="tech">T</a>
+                <a className="tech">T</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border project-wrap">
+              <h4 className="">Title</h4>
+              <p className="description">
+                Find out how we built a custom headless CMS with Node, Express,
+              </p>
+              <div className="technology">
+                <a className="tech">T</a>
+                <a className="tech">T</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border project-wrap">
+              <h4 className="">Title</h4>
+              <p className="description">
+                Find out how we built a custom headless CMS with Node, Express,
+                and Firebase for a project at Upstatement
+              </p>
+              <div className="technology">
+                <a className="tech">T</a>
+                <a className="tech">T</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border project-wrap">
+              <h4 className="">Title</h4>
+              <p className="description">
+                Find out how we built a custom headless CMS with Node, Express,
+                and Firebase for a project at Upstatement
+              </p>
+              <div className="technology">
+                <a className="tech">T</a>
+                <a className="tech">T</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className="footer"></div>
+    </section>
   </Layout>
 )
 
