3655797ba662cd5c096b991ddb9ee459ab129396
Adebayo Mohammed Olusiji <adebayomuhammad47@gmail.com>
2020-10-09 21:42:05 +0100

    feat: Project links

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,34 +14,35 @@ const About = () => (
             <h1>My Story</h1>
           </div>
           <div className="story-paragraph">
-            Adipiscing cras cursus eleifend est malesuada molestie nunc
-            phasellus porta posuere suspendisse ut. Aenean arcu at bibendum
-            egestas eget eros facilisi fringilla integer lectus mattis mollis
-            nisl pharetra risus semper torquent. Accumsan convallis cubilia
-            curae; dapibus dignissim dui duis eget facilisi fringilla iaculis
-            laoreet libero lobortis luctus mus parturient porta semper ultrices
-            vehicula velit vivamus viverra. Ac bibendum elit justo leo libero
-            magnis sagittis sociosqu ut vestibulum. Ante arcu convallis cras
-            dolor eros hac leo natoque nisl quis sociis ultricies. A ac
-            condimentum consectetur cras elementum facilisi iaculis justo
-            lacinia magnis mus parturient placerat platea rutrum sed semper
-            sociis sociosqu suspendisse tempus ullamcorper velit vestibulum
-            vitae vivamus. Aliquam condimentum curae; dictumst dui eu facilisis
-            hendrerit magnis nam non parturient tincidunt ultrices urna vel. Ad
-            adipiscing amet consequat convallis eget fames feugiat habitasse
-            himenaeos lectus ligula lobortis malesuada mi nibh nisl ornare
-            platea pulvinar rutrum sapien tellus torquent turpis vestibulum.
-            Adipiscing augue congue duis habitasse luctus massa metus nascetur
-            odio tempus urna vitae. Aenean blandit cum curabitur dolor iaculis
-            justo lobortis massa mauris molestie mollis nam non nunc odio orci
-            parturient pellentesque placerat porta quisque sodales ullamcorper
-            urna velit viverra. Adipiscing cras cursus eleifend est malesuada
-            molestie nunc phasellus porta posuere suspendisse ut. Aenean arcu at
-            bibendum egestas eget eros facilisi fringilla integer lectus mattis
-            mollis nisl pharetra risus semper torquent. Accumsan convallis
-            cubilia curae; dapibus dignissim dui duis eget facilisi fringilla
-            iaculis laoreet libero lobortis luctus mus parturient porta semper
-            ultrices vehicula velit vivamus viverra. Ac bibendum elit justo leo
+            Muhammad bashir is a Front-End Web Developer based in Nigeria. He
+            loves designing & coding simple aesthetic designs, playing football,
+            traveling, and watching movies. Presently, he works as a freelance
+            web developer. He has a BSc in Chemistry from the University of
+            Ilorin, a PGD and Masters Degree in Chemical and Polymer Engineering
+            at Lagos State University. For more info, check out his portfolio. I
+            have a diverse set of skills i employ in coding pixel perfect design
+            by writing simple and well structured HTML combined with reasonable
+            and efficient CSS with it various transpilers. i am also well versed
+            with A ac condimentum consectetur cras elementum facilisi iaculis
+            justo lacinia magnis mus parturient placerat platea rutrum sed
+            semper sociis sociosqu suspendisse tempus ullamcorper velit
+            vestibulum vitae vivamus. Aliquam condimentum curae; dictumst dui eu
+            facilisis hendrerit magnis nam non parturient tincidunt ultrices
+            urna vel. Ad adipiscing amet consequat convallis eget fames feugiat
+            habitasse himenaeos lectus ligula lobortis malesuada mi nibh nisl
+            ornare platea pulvinar rutrum sapien tellus torquent turpis
+            vestibulum. Adipiscing augue congue duis habitasse luctus massa
+            metus nascetur odio tempus urna vitae. Aenean blandit cum curabitur
+            dolor iaculis justo lobortis massa mauris molestie mollis nam non
+            nunc odio orci parturient pellentesque placerat porta quisque
+            sodales ullamcorper urna velit viverra. Adipiscing cras cursus
+            eleifend est malesuada molestie nunc phasellus porta posuere
+            suspendisse ut. Aenean arcu at bibendum egestas eget eros facilisi
+            fringilla integer lectus mattis mollis nisl pharetra risus semper
+            torquent. Accumsan convallis cubilia curae; dapibus dignissim dui
+            duis eget facilisi fringilla iaculis laoreet libero lobortis luctus
+            mus parturient porta semper ultrices vehicula velit vivamus viverra.
+            Ac bibendum elit justo leo
           </div>
         </div>
       </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,13 @@ import React from "react"
 // import { Link } from
 import Img from "gatsby-image"
 import { graphql, useStaticQuery } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faGithub,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -20,6 +27,8 @@ const IndexPage = () => {
             childImageSharp {
               fluid {
                 src
+                srcSet
+                base64
               }
             }
           }
@@ -30,35 +39,63 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <section id="hero" className="border">
+      <section id="hero" className="">
         <div className="sister"></div>
         <div className="container hero-header p-0">
-          <p>Hi, my name is</p>
-          <h2>
-            Muhammad Bashir. <br /> I build things for the web.
-          </h2>
-          <p className="hero-description">
-            I'm a front end developer based in Lagos, specializing <br />
-            in building exceptional websites, applications, and <br />
-            everything in between.
-          </p>
+          <div>
+            <p>Hi, my name is</p>
+            <h2>
+              <span style={{ fontWeight: `700` }}>Muhammad Bashir.</span> <br />{" "}
+              I build things for the web.
+            </h2>
+            <p className="hero-description">
+              I'm a frontend developer based in Lagos, specializing <br />
+              in building exceptional websites, applications, and <br />
+              everything in between.
+            </p>
+            <span className="  call-to-action ">Hire me na</span>
+          </div>
         </div>
+
         <span className="socials container ">
-          <li>GitHub</li>
-          <li>LinkedIn</li>
-          <li>Twitter</li>
+          <li>
+            <a href="https://github.com/Muhammadcod" className="git">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/adebayo-mohammed"
+              className="linked"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/adebayomuhamma2" className="twitter">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/olushiji24"
+              className="instagram"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </li>
         </span>
       </section>
 
-      <section className=" border">
+      <section className="">
         <div className="title p-3">Recent Works</div>
         <div className="container px-5 ">
           {data.allInfoJson.nodes.slice(1, 4).map(item => (
-            <div className="container way work">
+            <div className="container way  work">
               <div class="row  p-lg-2 p-sm-0 p-md-2 lo position-relative">
                 <div class="col-lg-6 col-12 tell two ">
                   <div className="work-card">
-                    <p className="work-title">{item.title}</p>
+                    <h5 className="work-title">{item.title}</h5>
 
                     <div className="des">
                       <p className="work-description">{item.description}</p>
@@ -66,9 +103,18 @@ const IndexPage = () => {
                   </div>
                   <div className="work-tech">
                     {item.tech.map(tec => (
-                      <li>{tec}</li>
+                      <li className="small">{tec.toLowerCase()}</li>
                     ))}
                   </div>
+                  <span className="d-flex">
+                    <span>
+                      <a href={item.url}>demo</a>
+                    </span>
+                    <span>
+                      {/* {console.log("repo", item)} */}
+                      <a href={item.repo}>code</a>
+                    </span>
+                  </span>
                 </div>
                 <div class="col-lg-6 col-12 border two sky">
                   <Img fluid={item.src.childImageSharp.fluid} alt="products" />
@@ -93,10 +139,18 @@ const IndexPage = () => {
                     <div className="technology">
                       <ul class="nav">
                         {item.tech.map(tec => (
-                          <li class="nav-item mr-3">{tec}</li>
+                          <li class="nav-item mr-3 work-tech">{tec}</li>
                         ))}
                       </ul>
                     </div>
+                    <span className="d-flex">
+                      <span>
+                        <a href={item.repourl}>code</a>
+                      </span>
+                      <span>
+                        <a href={item.url}>demo</a>
+                      </span>
+                    </span>
                   </div>
                 </div>
               ))}
